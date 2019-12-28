342fab5c83e018c8fb6e30ea481dff6646cb7a8e
Andrew Tropin <andrewtropin@gmail.com>
2019-12-28 12:40:54 +0300

    Add resources diff file

--- src/multiverse/fhir.clj
+++ src/multiverse/fhir.clj
@@ -7,6 +7,40 @@
   (let [fhir-version (str "fhir-" v)]
     (read-string (slurp (io/resource (str fhir-version ".edn"))))))
 
+(defn write-to-file [filename data]
+  (spit (str filename) data))
+
+
+
+(def r4 (load-version "4.0.0"))
+(def r3 (load-version "3.0.1"))
+
+(defn get-resources [fhir]
+  (->
+   fhir
+   (get-in [:resources :Entity])
+   keys
+   set))
+
+(let [r3-resources      (get-resources r3)
+      r4-resources      (get-resources r4)
+      added-resources   (clojure.set/difference r4-resources r3-resources)
+      deleted-resources (clojure.set/difference r3-resources r4-resources)
+      shared-resources  (clojure.set/intersection r3-resources r4-resources)
+      ]
+    (def diff
+      {:resources
+       {:added   added-resources
+        :deleted deleted-resources
+        :shared  shared-resources}})
+
+    (spit
+     "resources_diff.edn"
+     (with-out-str
+       (clojure.pprint/pprint diff)))
+    )
+(clojure.pprint/pprint diff)
+
 (comment
 
 
